fed5940dfba77622b436b70b4e1229c76a6acf42
Casper Yonel <yonelcasper@gmail.com>
2022-01-03 16:20:06 -0700

    theme added

---
+++ src/useTheme.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+
+const useTheme = (startingTheme = 'light') => {
+
+    const [theme, setTheme] = useState(startingTheme)
+
+    function validateTheme(themeValue) {
+        if (themeValue === 'dark') {
+            setTheme('dark')
+        } else {
+            setTheme('light')
+        }
+    }
+
+    return {
+        theme, 
+        setTheme: validateTheme,
+    }
+}
+
+export default useTheme
