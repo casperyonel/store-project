4c2efed976b0ebf0c0a19e4e6fe6e1f58d261ae9
Casper Yonel <yonelcasper@gmail.com>
2022-01-05 09:33:20 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import useTheme from './useTheme';
+import test from './test';
 
 function App() {
   return (
     <div className="App">
       <h2>test </h2>
+      <test />
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const test = () => {
+    return (
+        <div>
+           <h2>Heilllooo</h2>
+        </div>
+    )
+}
+
+export default test
